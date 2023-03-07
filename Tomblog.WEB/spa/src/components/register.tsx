d72ea6efffe2fa8c8470d86105754e13f0ca75be
Pankov Artem <fatly5454@gmail.com>
2023-03-07 18:37:38 +0600

    Добавлена форма регистрации

---
+++ Tomblog.WEB/spa/src/components/register.tsx
@@ -0,0 +1,48 @@
+import { Box, CardContent, TextField } from '@mui/material'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import React from 'react'
+import Center from './center'
+
+export default function Register() {
+    return (
+        <Center>
+            <Card sx={{ width: '400px' }}>
+                <CardContent sx={{textAlign: 'center'}}>
+                    <Box sx={{
+                        '& .MuiTextField-root': {
+                            margin: '5px',
+                            width: '90%'
+                        }
+                    }}>
+                        <form noValidate>
+                            <TextField
+                                multiline
+                                placeholder="Имя"
+                                label="Введите имя"
+                                name="name"
+                                variant="outlined" />
+                            <TextField
+                                multiline
+                                placeholder="Email"
+                                label="Email"
+                                name="email"
+                                variant="outlined" />
+                            <TextField
+                                multiline
+                                placeholder="Пароль"
+                                label="Введите пароль"
+                                name="password"
+                                variant="outlined" />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                sx={{ width: '90%' }}>Регистрация</Button>
+                        </form>
+                    </Box >
+                </CardContent>
+            </Card>
+        </Center>
+    )
+}
